a1482dfc9de529cadd7cde01dcbb2e1ce336cd98
tanmoypal003 <tanmoypal220@gmail.com>
2024-10-10 02:09:45 +0600

    skills section updated

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function sendMail(){
+function sendMail() {
     let params = {
         objective: document.getElementById("objective").value,
         name: document.getElementById("name").value,
         mail: document.getElementById("mail").value,
         mobile: document.getElementById("mobile").value,
         message: document.getElementById("message").value,
-    }
+    };
 
-    // emailjs.send("service_2l60bdh", "template_mw8o42m", params).then(alert("Email has been sent!"))
+
     emailjs.send("service_csgh3db", "template_mw8o42m", params).then(alert("Email has been sent!"));
 }
